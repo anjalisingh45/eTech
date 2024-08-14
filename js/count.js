f2d08e44139056ddc004887decfdf62021584f85
Anjali Singh <163300398+anjalisingh45@users.noreply.github.com>
2024-08-14 15:56:36 +0530

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,4 +1,4 @@
-const endDate = "27 July 2024 08:20:00 PM"
+const endDate = "27 July 2025 08:20:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
